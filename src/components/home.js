dfec9fe879d7b043bad6abe395996897fc387f5c
romson <romerolijkwan@gmail.com>
2019-05-30 21:37:58 +0200

    first commit

---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <section className="homeSection img-fluid">
+        <div className="homeSlogan w3-container w3-center w3-animate-bottom">
+          <h1>Company Name</h1>
+          <p>Slogan: These are the Best startup ideas in the World!</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar, Nav, Button } from "react-bootstrap";
+
+export const Navbarmenu = () => (
+  <Navbar className="navbarmenu" expand="lg">
+    <Navbar.Brand href="#home">
+      <img
+        className="logo"
+        src="https://tppwebsolutions.com/wp-content/uploads/logo-design.png"
+        alt="Company log"
+      />
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">HOME</Nav.Link>
+        <Nav.Link href="#features">PRICING</Nav.Link>
+        <Nav.Link href="#pricing">CONTACT</Nav.Link>
+      </Nav>
+      <Button className="buttonLogin" variant="outline-info">
+        <i className="fas fa-user" /> Login
+      </Button>
+      <Button variant="outline-info">Register</Button>
+    </Navbar.Collapse>
+  </Navbar>
+);
